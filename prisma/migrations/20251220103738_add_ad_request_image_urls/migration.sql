808e3d83edf326b95a7de058953f698260614b1d
bobabobov <bobov097@gmail.com>
2025-12-21 12:53:53 +0500

    ыыы
    ыы

--- prisma/migrations/20251220103738_add_ad_request_image_urls/migration.sql
+++ prisma/migrations/20251220103738_add_ad_request_image_urls/migration.sql
@@ -1,16 +1,3 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[googleId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-
-*/
 -- AlterTable
 ALTER TABLE "AdRequest" ADD COLUMN "imageUrls" JSONB;
 ALTER TABLE "AdRequest" ADD COLUMN "mobileBannerUrls" JSONB;
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "googleEmail" TEXT;
-ALTER TABLE "User" ADD COLUMN "googleId" TEXT;
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
